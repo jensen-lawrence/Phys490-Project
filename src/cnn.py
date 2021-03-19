3379bfd3e7943ee1749856ef2e66f5408e1fb005
Jensen Lawrence <jptlawrnce@gmail.com>
2021-03-19 18:38:40 -0400

    Created CNN script

---
+++ src/cnn.py
@@ -0,0 +1,19 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Imports
+# ----------------------------------------------------------------------------------------------------------------------
+
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalizer
+from tensorflow.keras import Sequential
+import tensorflow.keras.layers as layers
+from get_data import get_data
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Implementation of Convolution Neural Network
+# ----------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    pass
