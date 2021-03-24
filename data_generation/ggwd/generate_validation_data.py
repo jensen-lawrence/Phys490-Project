9a7b9d58a87bff7181c3a60de5b542a2aceeac8d
michael <mastwood101@gmail.com>
2021-03-24 19:20:15 -0400

    validation dataset generator

---
+++ data_generation/ggwd/generate_validation_data.py
@@ -0,0 +1,6 @@
+import subprocess
+subprocess.run(["python2","generate_sample.py","--config-file=params_v1.json"])
+subprocess.run(["python2","generate_sample.py","--config-file=params_v2.json"])
+subprocess.run(["python2","generate_sample.py","--config-file=params_v3.json"])
+subprocess.run(["python2","generate_sample.py","--config-file=params_v4.json"])
+subprocess.run(["python2","generate_sample.py","--config-file=params_v5.json"])
