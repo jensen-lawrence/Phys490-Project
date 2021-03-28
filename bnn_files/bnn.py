04621f6d366bf8f20a18e6a8f51d817d0324d82e
Madison Buitenhuis <mlbuiten@waterloo.ca>
2021-03-28 01:27:53 -0400

    updated implementation of bnn

--- bnn_files/bnn.py
+++ bnn_files/bnn.py
@@ -19,10 +19,15 @@ class bnn(nn.Module):
     def __init__(self, input_dim, output_dim):
         super().__init__()
         self.linear = nn.Linear(input_dim, output_dim)
-        self.blinear1 = BayesianLinear(input_dim, 64)
-        self.blinear2 = BayesianLinear(64, output_dim)
+        self.blinear1 = BayesianLinear(input_dim, 80)
+        self.blinear2 = BayesianLinear(80, output_dim)
         
     def forward(self, x):
         x_ = self.linear(x)
         x_ = self.blinear1(x_)
         return self.blinear2(x_)
+
+    
+    def reset(self):
+        self.fc1.reset_parameters()
+        self.fc2.reset_parameters()
