61437de74718ef15fe6fdc2a30f5caba5d69ed80
Jensen Lawrence <jptlawrnce@gmail.com>
2021-04-02 18:40:01 -0400

    Finished CNN framework

--- cnn_files/cnn.py
+++ cnn_files/cnn.py
@@ -2,19 +2,17 @@
 # Imports
 # ----------------------------------------------------------------------------------------------------------------------
 
+import sys
 import json
 import argparse
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalizer
+from sklearn.preprocessing import normalize
+import tensorflow as tf
 from tensorflow.keras import Sequential
-import tensorflow.keras.layers as layers
+from tensorflow.keras.layers import Activation, Conv1D, Dense, Dropout, Flatten, MaxPool1D
+sys.path.append('../src')
 from get_data import get_data
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-# Data Processing
-# ----------------------------------------------------------------------------------------------------------------------
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -22,13 +20,63 @@ from get_data import get_data
 # ----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Description goes here')
-    parser.add_argument('--param', type='str')
-    parser.add_argument('--data', type='str')
-    parser.add_argument('-v', type=int)
 
+    # Implementing argument parser
+    parser = argparse.ArgumentParser(description='Description goes here')
+    parser.add_argument('--param', type=str)
+    parser.add_argument('--data', type=str)
+    parser.add_argument('-v', type=int)
+    args = parser.parse_args()
+
+    # Loading neural network parameters
     with open(args.param) as f:
         nn_params = json.load(f)
     f.close()
 
+    n_train = nn_params['n_train']
+    n_valid = nn_params['n_valid']
+    dropout = nn_params['dropout']
+    learn_rate = nn_params['learn_rate']
+    n_epochs = nn_params['n_epochs']
+    batch_size = nn_params['batch_size']
+
+    # Loading training data
+    X, y = get_data(args.data, n_train + n_valid)
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=n_valid)
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+    X_valid = X_valid.reshape(X_valid.shape[0], X_valid.shape[1], 1)
+    y_train = y_train.reshape(y_train.size, 1, 1)
+    y_valid = y_valid.reshape(y_valid.size, 1, 1)
+    input_shape = X_train.shape[1:]
+
+    # Initializing convolutional neural network
     model = Sequential()
+    model.add(Conv1D(16, 16, input_shape=input_shape))
+    model.add(MaxPool1D(pool_size=4))
+    model.add(Activation(tf.keras.activations.relu))
+    model.add(Conv1D(32, 8))
+    model.add(MaxPool1D(pool_size=4))
+    model.add(Activation(tf.keras.activations.relu))
+    model.add(Conv1D(64, 8))
+    model.add(MaxPool1D(pool_size=4))
+    model.add(Activation(tf.keras.activations.relu))
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Dropout(rate=dropout))
+    model.add(Activation(tf.keras.activations.relu))
+    model.add(Dense(1))
+
+    # Initializing loss function, optimizer, and performance metrics
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    optimizer = tf.keras.optimizers.Adam(lr=learn_rate)
+    metrics = ['accuracy', 'AUC']
+
+    # Training and evaluating model
+    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+    training_results = model.fit(X_train, y_train, epochs=n_epochs, batch_size=batch_size, verbose=args.v, validation_data=(X_valid, y_valid))
+    print(training_results.history)
+
+    # model.evaluate(X_test, y_test, batch_size=batch_size, verbose=args.v)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
