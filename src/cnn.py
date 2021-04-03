54a2cd6b9d35f0dd834d5f357e8679b918527fa7
catieterrey <mastwood101@gmail.com>
2021-04-02 21:55:22 -0400

    batch normalization

--- src/cnn.py
+++ src/cnn.py
@@ -11,7 +11,9 @@ from sklearn.preprocessing import normalize
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Activation, Conv1D, Dense, Dropout, Flatten, MaxPool1D
+from tensorflow.keras.layers import BatchNormalization
 sys.path.append('.')
+
 from get_data import get_data
 
 
@@ -59,16 +61,20 @@ if __name__ == '__main__':
     model.add(Conv1D(16, 16, input_shape=input_shape))
     model.add(MaxPool1D(pool_size=4))
     model.add(Activation(tf.keras.activations.relu))
+    model.add(BatchNormalization())
     model.add(Conv1D(32, 8))
     model.add(MaxPool1D(pool_size=4))
     model.add(Activation(tf.keras.activations.relu))
+    model.add(BatchNormalization())
     model.add(Conv1D(64, 8))
     model.add(MaxPool1D(pool_size=4))
     model.add(Activation(tf.keras.activations.relu))
+    model.add(BatchNormalization())
     model.add(Flatten())
     model.add(Dense(64))
     model.add(Dropout(rate=dropout))
     model.add(Activation(tf.keras.activations.relu))
+    model.add(BatchNormalization())
     model.add(Dense(1))
     model.add(Activation(tf.keras.activations.sigmoid))
 
