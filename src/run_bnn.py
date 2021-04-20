c2cec1d6f4da1e146513c5833e4b038ce6028a0f
catieterrey <caterrey@uwaterloo.ca>
2021-04-19 22:51:10 -0400

    small changes to bnn

--- src/run_bnn.py
+++ src/run_bnn.py
@@ -13,8 +13,10 @@ import torch.functional as F
 from blitz.utils import variational_estimator
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.preprocessing import normalize
 from bnn import bnn
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Run Data Through BNN model
@@ -26,22 +28,26 @@ def acc(labels,output):
     numequal = np.sum(np.equal(output,labels).astype(int))
     return numequal/labels.size
 
-def run_bnn(param):
+def bnn_train(param,data_path,v,results_dir):
+    with open(param) as f:
+        params = json.load(f)
+    f.close()
     device = torch.device(params['device'])
     n_valid = params['n_valid']
     batch_size=params['batch_size']
-    signals, labels = get_data(args.data, 5500)
+    signals, labels = get_data(args.train, 5500)
+    signals=normalize(signals)
     x_train, x_valid, y_train, y_valid = train_test_split(signals, labels, test_size=n_valid)
     train_in = torch.from_numpy(x_train.reshape(x_train.shape[0], 1, x_train.shape[1])).float().to(device)
     test_in = torch.from_numpy(x_valid.reshape(x_valid.shape[0], 1, x_valid.shape[1])).float().to(device)
     train_out = torch.from_numpy(y_train.reshape(y_train.size, 1)).float().to(device)
     test_out = torch.from_numpy(y_valid.reshape(y_valid.size, 1)).float().to(device)
     
-    lr = param['learn_rate']
-    batch_size = param['batch_size']
-    num_epoch = param['n_epoch']
-    display_epoch = param['display_epoch']
-
+    lr = params['learn_rate']
+    batch_size = params['batch_size']
+    num_epoch = params['n_epoch']
+    display_epoch = params['display_epoch']
+    cw=1e-7
     sample_nbr = 3
 
     model = bnn().to(device)
@@ -56,20 +62,22 @@ def run_bnn(param):
     loss_vals=[]
     accuracy_vals=[]
     auc_vals=[]
+    train_preds=[]
 
     test_loss_vals=[]
     test_acc_vals=[]
     test_auc_vals=[]
+    test_preds=[]
     for epoch in range(num_epoch):
         loss_val=0
         acc_val=0
         for i, (signals_t, labels_t) in enumerate(train_data):
             out = model.forward(signals_t)
-            predictions=torch.round(out).cpu().detach().numpy().astype(int)
-            auc_val = roc_auc_score(predictions,labels_t.cpu().detach().numpy())
-
+            predictions_t=torch.round(out).cpu().detach().numpy().astype(int)
+            train_preds.append(predictions_t)
+            auc_val = roc_auc_score(predictions_t,labels_t.cpu().detach().numpy())
             optimizer.zero_grad()  
-            loss = model.sample_elbo(signals_t, labels_t, loss_func, sample_nbr)
+            loss = model.sample_elbo(signals_t, labels_t, loss_func, sample_nbr,complexity_cost_weight=cw)
             loss.backward()
             optimizer.step() 
             loss_val+=loss.item()/(batch_size*len(train_data))
@@ -78,35 +86,64 @@ def run_bnn(param):
         auc_vals.append(auc_val)
         test_accuracy=0
         test_loss=0
+        test_auc_val=0
         with torch.no_grad():
             for (signals_test, labels_test) in test_data:
-                signals_test=signals_test.to(device)
-                labels_test=labels_test.to(device)
-                output = model.forward(signals_t)
-                predictions=torch.round(output).cpu().detach().numpy().astype(int)
-                test_auc_val = roc_auc_score(predictions,labels_test.cpu().detach().numpy())
-                test_loss += model.sample_elbo(signals_test, labels_test, loss_func, sample_nbr)/(batch_size*len(test_data))
-                test_accuracy += acc(labels_test, output)/len(test_data) 
+                meanpreds,stdpreds = model.mfvi_forward(signals_test,sample_nbr)
+                print(meanpreds.cpu().detach().numpy().shape,stdpreds.cpu().detach().numpy().shape)
+                #predictions=torch.round(output).cpu().detach().numpy().astype(int)
+                test_preds.append(meanpreds)
+                test_auc_val += roc_auc_score(np.column_stack((torch.round(meanpreds).cpu().detach().numpy(),stdpreds.cpu().detach().numpy())),
+                    labels_test.cpu().detach().numpy())/len(test_data) 
+                test_loss += model.sample_elbo(signals_test, labels_test, loss_func, sample_nbr,complexity_cost_weight=cw)/(batch_size*len(test_data))
+                test_accuracy += acc(labels_test, meanpreds)/len(test_data) 
         test_acc_vals.append(test_accuracy)
         test_auc_vals.append(test_auc_val)
-        if epoch % 5 == 0 and epoch<21:
-            lr /= 10.0
+        if epoch % 5 == 0:
+            lr /= 2.0
             for g in optimizer.param_groups:
                 g['lr'] = lr
 
         #if epoch % display_epoch==1:
-        print("Training Loss: {:.4f}".format(loss_val))
-        print("Test Loss: {:.4f}".format(test_loss))
-        print("Training Accuracy: {:.4f}".format(acc_val))
-        print("Test Accuracy: {:.4f}".format(test_accuracy))
-        print("-"*40)
-    
+        if args.v >0:
+            print("Epoch: {} - Training Loss: {:.4f} - Test Loss: {:.4f}".format(epoch,loss_val,test_loss))
+            print("Training Accuracy: {:.4f} - Test Accuracy: {:.4f} - Test AUC: {:.4f}".format(acc_val,test_accuracy,test_auc_val))
+            print("-"*40)
+
     print("Final Training Loss: {:.4f}".format(loss))
     print("Final Test Loss: {:.4f}".format(test_loss))
     training_results = {'train_loss': loss_vals,'test_loss':test_loss_vals,
         'train_acc':accuracy_vals,'test_acc':test_acc_vals,
         'train_auc':auc_vals,'test_auc':test_auc_vals}
+
+    
+
     return training_results
+
+def plot_bnn_roc(y_train, train_pred, y_valid, valid_pred, save_as):
+    # Getting training ROC curve
+    train_roc = roc_curve(y_train, train_pred)
+    fpr_train = train_roc[0]
+    tpr_train = train_roc[1]
+
+    # Getting validation ROC curve
+    valid_roc = roc_curve(y_valid, valid_pred)
+    fpr_valid = valid_roc[0]
+    tpr_valid = valid_roc[1]
+
+    # Plotting ROC curves
+    print('Plotting ROC curves...')
+    sns.set_style('whitegrid')
+    plt.figure(figsize=(12, 8))
+    plt.plot(fpr_train, tpr_train, color='dodgerblue', label='Training')
+    plt.plot(fpr_valid, tpr_valid, color='springgreen', label='Validation')
+    plt.xlabel('False Positive Rate', fontsize=16)
+    plt.ylabel('True Positive Rate', fontsize=16)
+    plt.legend(loc='best', fontsize=16)
+    plt.savefig(save_as + 'bnn_roc_curve.png', dpi=400)
+    print('Plotted training and validation ROC curves.')
+    print('-'*80)
+    plt.close()
 
 def plot_bnn(training_params,testing_params):
     train_loss=training_params['train_loss']
@@ -115,6 +152,42 @@ def plot_bnn(training_params,testing_params):
     test_acc=training_params['test_acc']
     train_auc=training_params['train_auc']
     test_auc=training_params['test_auc']
+
+    epochs = range(1, len(train_loss) + 1)
+    sns.set_style('whitegrid')
+
+    # Plotting loss
+    print('Plotting performance metrics...')
+    plt.figure(figsize=(12, 8))
+    ax1 = sns.lineplot(x=epochs, y=train_loss, color='dodgerblue', label='Training')
+    ax1 = sns.lineplot(x=epochs, y=valid_loss, color='springgreen', label='Validation')
+    ax1.set_xlabel('Epochs', fontsize=16)
+    ax1.set_ylabel('Loss', fontsize=16)
+    ax1.legend(loc='best', fontsize=16)
+    plt.savefig(save_as + 'bnn_loss.png', dpi=400)
+    print('Plotted training and validation loss.')
+
+    # Plotting accuracy
+    plt.figure(figsize=(12, 8))
+    ax2 = sns.lineplot(x=epochs, y=train_acc, color='dodgerblue', label='Training')
+    ax2 = sns.lineplot(x=epochs, y=valid_acc, color='springgreen', label='Validation')
+    ax2.set_xlabel('Epochs', fontsize=16)
+    ax2.set_ylabel('Accuracy', fontsize=16)
+    ax2.legend(loc='best', fontsize=16)
+    plt.savefig(save_as + 'bnn_accuracy.png', dpi=400)
+    print('Plotted training and validation accuracy.')
+
+    # Plotting AUC score
+    plt.figure(figsize=(12, 8))
+    ax3 = sns.lineplot(x=epochs, y=train_auc, color='dodgerblue', label='Training')
+    ax3 = sns.lineplot(x=epochs, y=valid_auc, color='springgreen', label='Validation')
+    ax3.set_xlabel('Epochs', fontsize=16)
+    ax3.set_ylabel('AUC Score', fontsize=16)
+    ax3.legend(loc='best', fontsize=16)
+    plt.savefig(save_as + 'bnn_auc_score.png', dpi=400)
+    print('Plotted training and validation AUC score.')
+    print('-'*80)
+    plt.close()
 # ----------------------------------------------------------------------------------------------------------------------
 # Implementation of Bayesian Neural Network
 # ----------------------------------------------------------------------------------------------------------------------
@@ -122,10 +195,8 @@ def plot_bnn(training_params,testing_params):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Description goes here')
     parser.add_argument('--param',default='../param/bnn_params.json', type=str, help='Path where params are stored')
-    parser.add_argument('--data',default='F:\phys490_data\data_1_training',type=str, help='Path where data is stored')
+    parser.add_argument('--train',default='F:\phys490_data\data_1_training',type=str, help='Path where data is stored')
+    parser.add_argument('--res',default='../results',type=str, help='Path where results are stored')
+    parser.add_argument('-v',default=2,type=int, help='Verbosity')
     args=parser.parse_args()
-    with open(args.param) as f:
-        params = json.load(f)
-    f.close()
-
-    run_bnn(params)
+    bnn_train(args.param,args.v,args.train,args.res)
